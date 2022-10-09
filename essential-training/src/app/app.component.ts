3e68001579ab02a229ae3c8754b818cc6982dfce
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-10-08 22:12:54 -0500

    [sm] continuing on essntial training built components testing mounting point in html etc

---
+++ essential-training/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-app', // Targets the <app-root> element in index.html.
+  template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent {}
