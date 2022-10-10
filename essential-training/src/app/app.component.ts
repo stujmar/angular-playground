898874058b8e939165440d374d7228b82e8475e5
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-10-09 19:54:40 -0500

    [sm] connected a basic html template and an external modularized css file

--- essential-training/src/app/app.component.ts
+++ essential-training/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app', // Targets the <app-root> element in index.html.
-  template: '<h1>My First Angular App</h1>'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
