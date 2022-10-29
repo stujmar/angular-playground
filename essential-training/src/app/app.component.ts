19cdcdf96ae6a2bb1fe90e9f8a8544440b2da7ad
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-10-28 21:02:39 -0500

    [sm] good gravy did I have trouble sorting out some css issues between the list and list-items

--- essential-training/src/app/app.component.ts
+++ essential-training/src/app/app.component.ts
@@ -1,56 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mw-app', // Targets the <app-root> element in index.html.
+  selector: 'mw-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  mediaItems = [
-    {
-      id: 1,
-      name: 'Firebug',
-      medium: 'Series',
-      category: 'Science Fiction',
-      year: 2010,
-      watchedOn: 1294166565384,
-      isFavorite: false
-    },
-    {
-      id: 2,
-      name: 'The Small Tall',
-      medium: 'Movies',
-      category: 'Comedy',
-      year: 2015,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 3,
-      name: 'The Redemption',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false
-    }, {
-      id: 4,
-      name: 'Hoopers',
-      medium: 'Series',
-      category: 'Drama',
-      year: null,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 5,
-      name: 'Happy Joe: Cheery Road',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false
-    }
-  ];
-
-  onMediaItemDelete(mediaItem) {}
-
-}
+export class AppComponent {}
--- essential-training/src/app/media-item.component.ts
+++ essential-training/src/app/media-item.component.ts
@@ -6,8 +6,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
-  // @Input('mediaItemToWatch') mediaItem; 'mediaItemToWatch' is the alias name of the property that will be used in the parent component. avoid this if possible?
-  @Input() mediaItem; 'mediaItemToWatch'
+  @Input() mediaItem; // 'mediaItemToWatch'
   @Output() delete = new EventEmitter(); // can alias just like the input property, but it's not necessary
 
   onDelete() {
@@ -15,4 +14,3 @@ export class MediaItemComponent {
     this.delete.emit(this.mediaItem);
   }
 }
-
